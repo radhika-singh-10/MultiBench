981cb74b3bc94487fdbc5d9f2bec7ffaf0612f11
radhika-singh-10 <rsingh57@buffalo.edu>
2025-01-16 18:46:54 -0500

    audio file logic

--- datasets/affect/get_data.py
+++ datasets/affect/get_data.py
@@ -4,11 +4,14 @@ import sys
 from typing import *
 import pickle
 import h5py
+import tqdm
 import numpy as np
 from numpy.core.numeric import zeros_like
 from torch.nn.functional import pad
 from torch.nn import functional as F
-
+import requests
+import robustness
+print(robustness.__file__)
 #sys.path.append(os.getcwd())
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 import torch
@@ -16,9 +19,269 @@ import torchtext as text
 from collections import defaultdict
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import DataLoader, Dataset
-from robustness import text_robust,timeseries_robust
-from robustness.text_robust import add_text_noise
-from robustness.timeseries_robust import add_timeseries_noise
+# from robustness.text_robust import add_text_noise
+# from robustness.timeseries_robust import add_timeseries_noise
+
+##############################################################################
+# Time-Series
+def add_timeseries_noise(tests, noise_level=0.3, gaussian_noise=True, rand_drop=True, struct_drop=True):
+    """
+    Add various types of noise to timeseries data.
+    
+    :param noise_level: Standard deviation of gaussian noise, and drop probability in random drop and structural drop
+    :param gauss_noise:  Add Gaussian noise to the time series ( default: True )
+    :param rand_drop: Add randomized dropout to the time series ( default: True )
+    :param struct_drop: Add randomized structural dropout to the time series ( default: True )
+    """
+    # robust_tests = np.array(tests)
+    robust_tests = tests
+    if gaussian_noise:
+        robust_tests = white_noise(robust_tests, noise_level)
+    if rand_drop:
+        robust_tests = random_drop(robust_tests, noise_level)
+    if struct_drop:
+        robust_tests = structured_drop(robust_tests, noise_level)
+    return robust_tests
+
+
+def white_noise(data, p):
+    """Add noise sampled from zero-mean Gaussian with standard deviation p at every time step.
+    
+    :param data: Data to process.
+    :param p: Standard deviation of added Gaussian noise.
+    """
+    for i in range(len(data)):
+        for time in range(len(data[i])):
+            data[i][time] += np.random.normal(0, p)
+    return data
+
+
+
+
+def random_drop(data, p):
+    """Drop each time series entry independently with probability p.
+    
+    :param data: Data to process.
+    :param p: Probability to drop feature.
+    """
+    for i in range(len(data)):
+        data[i] = _random_drop_helper(data[i], p, len(np.array(data).shape))
+    return data
+
+
+def _random_drop_helper(data, p, level):
+    """
+    Helper function that implements random drop for 2-/higher-dimentional timeseris data.
+
+    :param data: Data to process.
+    :param p: Probability to drop feature.
+    :param level: Dimensionality.
+    """
+    if level == 2:
+        for i in range(len(data)):
+            if np.random.random_sample() < p:
+                data[i] = 0
+        return data
+    else:
+        for i in range(len(data)):
+            data[i] = _random_drop_helper(data[i], p, level - 1)
+        return data
+
+
+def structured_drop(data, p):
+    """Drop each time series entry independently with probability p, but drop all modalities if you drop an element.
+    
+    :param data: Data to process.
+    :param p: Probability to drop entire element of time series.
+    """
+    for i in range(len(data)):
+        for time in range(len(data[i])):
+            if np.random.random_sample() < p:
+                data[i][time] = np.zeros(data[i][time].shape)
+    return data
+def add_text_noise(tests, noise_level=0.3, swap=True, rand_mid=True, typo=True, sticky=True, omit=True):
+    """
+    Add various types of noise to text data.
+    
+    :param noise_level: Probability of randomly applying noise to a word. ( default: 0.1)
+    :param swap:  Swap two adjacent letters. ( default: True )
+    :param rand_mid: Randomly permute the middle section of the word, except for the first and last letters. ( default: True )
+    :param typo: Simulate keyboard typos for the word. ( default: True )
+    :param sticky: Randomly repeat letters inside a word. ( default: True )
+    :param omit: Randomly omit some letters from a word ( default: True )
+    """
+    noises = []
+    if swap:
+        noises.append(swap_letter)
+    if rand_mid:
+        noises.append(random_mid)
+    if typo:
+        noises.append(qwerty_typo)
+    if sticky:
+        noises.append(sticky_keys)
+    if omit:
+        noises.append(omission)
+    robustness_tests = []
+    for i in tqdm(range(len(tests))):
+        newtext = []
+        text = _normalizeText(tests[i])
+        for word in text:
+            if _last_char(word) > 3 and np.random.sample() <= noise_level:
+                mode = np.random.randint(len(noises))
+                newtext.append(noises[mode](word))
+            else:
+                newtext.append(word)
+        robustness_tests.append(' '.join(newtext))
+    return robustness_tests
+
+
+def _normalizeText(text):
+    """Normalize text before transforming."""
+    text = text.lower()
+    text = re.sub(r'<br />', r' ', text).strip()
+    text = re.sub(r'^https?:\/\/.*[\r\n]*', ' L ', text, flags=re.MULTILINE)
+    text = re.sub(r'[\~\*\+\^`_#\[\]|]', r' ', text).strip()
+    text = re.sub(r'[0-9]+', r' N ', text).strip()
+    text = re.sub(r'([/\'\-\.?!\(\)",:;])', r' \1 ', text).strip()
+    return text.split()
+
+
+def _last_char(word):
+    """Get last alphanumeric character of word.
+    
+    :param word: word to get the last letter of.
+    """
+    for i in range(len(word)):
+        if word[len(word)-1-i].isalpha() or word[len(word)-1-i].isdigit():
+            return len(word) - 1 - i
+    return -1
+
+
+def swap_letter(word):
+    """Swap two random adjacent letters.
+    
+    :param word: word to apply transformations to.
+    """
+    last = _last_char(word)
+    pos = np.random.randint(last-2) + 1
+    return word[:pos] + word[pos+1] + word[pos] + word[pos+2:]
+
+
+def random_mid(word):
+    """Randomly permute the middle chunk of a word (all letters except the first and last letter).
+    
+    :param word: word to apply transformations to.
+    """
+    last = _last_char(word)
+    mid = [char for char in word[1:last]]
+    np.random.shuffle(mid)
+    return word[0]+''.join(mid)+word[last:]
+
+
+def qwerty_typo(word):
+    """Randomly replace num_typo number of letters of a word to a one adjacent to it on qwerty keyboard.
+    
+    :param word: word to apply transformations to.:
+    """
+    qwerty = {'q': ['w'], 'w': ['q', 'e', 's'], 'e': ['w', 'r', 'd'], 'r': ['e', 't', 'f'], 't': ['r', 'g', 'y'], 'y': ['t', 'u', 'h'], 'u': ['y', 'i', 'j'], 'i': ['u', 'o', 'k'], 'o': ['i', 'p', 'l'], 'p': ['o'], 'a': ['q', 's', 'z'], 's': ['a', 'w', 'd', 'x', 'z'], 'd': ['s', 'e', 'f', 'x', 'c'], 'f': ['d', 'r', 'g', 'c', 'v'], 'g': [
+        'f', 't', 'h', 'v', 'b'], 'h': ['g', 'y', 'j', 'b', 'n'], 'j': ['h', 'u', 'k', 'n', 'm'], 'k': ['j', 'i', 'l', 'm'], 'l': ['k', 'o'], 'z': ['a', 's', 'x'], 'x': ['z', 's', 'd', 'c'], 'c': ['x', 'd', 'f', 'v'], 'v': ['c', 'f', 'g', 'b'], 'b': ['v', 'g', 'h', 'n'], 'n': ['b', 'h', 'm', 'j'], 'm': ['n', 'j', 'k']}
+    last = _last_char(word)
+    typos = np.arange(last+1)
+    np.random.shuffle(typos)
+    for i in range(len(typos)):
+        if word[typos[i]] in qwerty:
+            typo = qwerty[word[typos[i]]]
+            key = typo[np.random.randint(len(typo))]
+            word = word[:typos[i]] + key + word[typos[i]+1:]
+            break
+    return word
+
+
+def sticky_keys(word, num_sticky=1):
+    """Randomly repeat letters of a word once.
+    
+    :param word: word to apply transformations to.
+    :param num_sticky: Number of letters to randomly repeat once.
+    """
+    last = _last_char(word)
+    sticky = np.arange(last+1)
+    np.random.shuffle(sticky)
+    for i in range(num_sticky):
+        word = word[:sticky[i]] + word[sticky[i]] + word[sticky[i]:]
+    return word
+
+
+def omission(word, num_omit=1):
+    """Randomly omit num_omit number of letters of a word.
+    
+    :param word: word to apply transformations to.
+    :param num_sticky: Number of letters to randomly omit.
+    """
+    last = _last_char(word)
+    for i in range(num_omit):
+        omit = np.random.randint(last-1) + 1
+        word = word[:omit] + word[omit+1:]
+        last -= 1
+    return word
+
+
+def add_audio_noise(tests, noise_level=0.3, noises=None):
+    """
+    Add various types of noise to audio data.
+
+    :param noise_level: Probability of randomly applying noise to each audio signal, and standard deviation for gaussian noise, and structured dropout probability.
+    :param noises: list of noises to add. # TODO: Change this to use either a list of enums or if statements.
+    """
+    if noises is None:
+        noises = [additive_white_gaussian_noise,
+                  audio_random_dropout, audio_structured_dropout]
+    robustness_tests = np.zeros(tests.shape)
+    for i in range(len(tests)):
+        if np.random.sample() <= noise_level:
+            mode = np.random.randint(len(noises))
+            robustness_tests[i] = noises[mode](tests[i], noise_level)
+    return robustness_tests
+
+
+def additive_white_gaussian_noise(signal, noise_level):
+    """
+    Add gaussian white noise to audio signal.
+
+    :param signal: Audio signal to permute.
+    :param noise_level: standard deviation of the gaussian noise.
+    """
+    # SNR = 10 * log((RMS of signal)^2 / (RMS of noise)^2)
+    # RMS_s = np.sqrt(np.mean(signal*signal))
+    # RMS_n = np.sqrt(RMS_s*RMS_s / (np.power(10, SNR/10)))
+    noise = np.random.normal(0, noise_level, signal.shape[0])
+    return signal + noise
+
+
+def audio_structured_dropout(signal, p, step=10):
+    """
+    Randomly drop signal for `step` time steps.
+
+    :param signal: Audio signal to permute.
+    :param p: Dropout probability.
+    :param step: Number of time steps to drop the signal.
+    """
+    res = [signal[i] for i in range(len(signal))]
+    for i in range(len(res)-step+1):
+        if (res[i] != 0) and np.random.random_sample() < p:
+            for j in range(step):
+                res[i+j] = 0
+    return res
+
+
+def audio_random_dropout(sig, p):
+    """
+    Randomly drop the signal for a single time step.
+
+    :param signal: Audio signal to transform.
+    :param p: Dropout probability.
+    """
+    return audio_structured_dropout(sig, 1, p)
+
 
 np.seterr(divide='ignore', invalid='ignore')
 
@@ -453,19 +716,19 @@ def get_dataloader(
         #     test.append(alldata_test)
 
         robust_timeseries = []
-        # alldata['test'] = drop_entry(alldata['test'])
+        alldata['test'] = drop_entry(alldata['test'])
         for i in range(10):
-            robust_timeseries_tmp = add_timeseries_noise(
-                [alldata['test']['vision'].copy(), alldata['test']['audio'].copy(), alldata['test']['text'].copy()],
-                noise_level=i / (10 * 3), rand_drop=False)
+            # robust_timeseries_tmp = add_timeseries_noise(
+            #     [alldata['test']['vision'].copy(), alldata['test']['audio'].copy(), alldata['test']['text'].copy()],
+            #     noise_level=i / (10 * 3), rand_drop=False)
             
             test = dict()
-            test['vision'] = robust_timeseries_tmp[0]
-            test['audio'] = robust_timeseries_tmp[1]
-            test['text'] = robust_timeseries_tmp[2]
+            #test['vision'] = robust_timeseries_tmp[0]
+            # test['audio'] = robust_timeseries_tmp[1]
+            # test['text'] = robust_timeseries_tmp[2]
             test['labels'] = alldata['test']['labels']
             test = drop_entry(test)
-            print('test entries: {}'.format(test['vision'].shape))
+            # print('test entries: {}'.format(test['vision'].shape))
 
             robust_timeseries.append(
                 DataLoader(Affectdataset(test, flatten_time_series, task=task, max_pad=max_pad, max_pad_num=max_seq_len, data_type=data_type, z_norm=z_norm), shuffle=False, num_workers=num_workers,
